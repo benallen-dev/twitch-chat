69928a78973ab877ffccc33de043eea1573899de
Ben Allen <ben@benallen.dev>
2024-06-22 12:12:26 +0200

    Tidy up more

--- main.go
+++ main.go
@@ -12,8 +12,7 @@ import (
 
 func main() {
 	// Hide cursor and clear screen
-	fmt.Print("\033[?25l")
-	fmt.Print("\033[2J\033[H")
+	fmt.Print("\033[?25l\033[2J\033[H")
 
 	// Default to my twitch channel
 	var channel = "stevemacawesome"
@@ -33,16 +32,18 @@ func main() {
 		usr := message.User.DisplayName
 		msg := message.Message
 		col := message.User.Color
-		r, g, b := int64(49), int64(154), int64(36) // Same default as Twitch
+		
+		// Same default color as Twitch
+		r, g, b := int64(49), int64(154), int64(36) 
 		if col != "" {
 			r, _ = strconv.ParseInt(col[1:3], 16, 64)
 			g, _ = strconv.ParseInt(col[3:5], 16, 64)
 			b, _ = strconv.ParseInt(col[5:7], 16, 64)
 		}
 
-		colorCode := fmt.Sprintf("\033[0m\033[38;2;%d;%d;%dm", r, g, b)
+		ansiCol := fmt.Sprintf("\033[38;2;%d;%d;%dm", r, g, b)
 
-		fmt.Printf("%s%s\033[0m: %s\n", colorCode, usr, msg)
+		fmt.Printf("%s%s\033[0m: %s\n", ansiCol, usr, msg)
 	})
 
 	// Restore cursor on exit
@@ -50,7 +51,7 @@ func main() {
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c
-		fmt.Print("\033[?25h") // Show cursor
+		fmt.Print("\033[?25h")
 		os.Exit(1)
 	}()
 
