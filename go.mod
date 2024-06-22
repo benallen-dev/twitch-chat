19a2c279a8f6193e905a5504b46a4465287ae158
Ben Allen <ben@benallen.dev>
2024-06-22 12:03:02 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/benallen-dev/twitch-chat
+
+go 1.22.4
+
+require github.com/gempir/go-twitch-irc/v4 v4.0.0
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+	"fmt"
+	"strconv"
+
+	twitch "github.com/gempir/go-twitch-irc/v4"
+)
+
+
+func main () {
+	fmt.Print("\033[?25l") // Hide cursor
+	fmt.Print("\033[2J\033[H") // Clear screen
+
+	var channel = "stevemacawesome"
+	if len(os.Args) > 1 {
+		channel = os.Args[1]
+	}
+
+	client := twitch.NewAnonymousClient()
+	client.Join(channel)
+	
+	client.OnConnect(func() {
+		fmt.Println("Connected to", channel)
+	})
+
+	// Private message is a misnomer, it's actually a chat message
+	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
+		usr := message.User.DisplayName
+		msg := message.Message
+		col := message.User.Color
+		r, g, b := int64(49), int64(154), int64(36) // Same default as Twitch
+		if col != "" {
+			r, _ = strconv.ParseInt(col[1:3], 16, 64)
+			g, _ = strconv.ParseInt(col[3:5], 16, 64)
+			b, _ = strconv.ParseInt(col[5:7], 16, 64)
+		}
+
+		colorCode := fmt.Sprintf("\033[0m\033[38;2;%d;%d;%dm", r,g,b);
+
+		fmt.Printf("%s%s\033[0m: %s\n", colorCode, usr, msg)
+	})
+
+	// Restore cursor on exit
+	c := make(chan os.Signal, 1)
+    signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+    go func() {
+        <-c
+		fmt.Print("\033[?25h") // Show cursor
+        os.Exit(1)
+    }()
+
+
+	err := client.Connect()
+	if err != nil {
+		panic(err)
+	}
+}
